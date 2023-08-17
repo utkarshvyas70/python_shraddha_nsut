e50631d15e802505c0ee7724e27059fb2e779667
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2023-08-18 01:01:01 +0530

    adding py file

---
+++ typeconversion.py
@@ -0,0 +1,14 @@
+old_age=input("Enter your old age: ")
+# new_age=(old_age)+2
+new_age=int(old_age)+2
+print(new_age)
+
+number=18
+g=float(number)
+print(g)
+
+first=input("Enter first number: ")
+second=input("Enter second number: ")
+sum=int(first)+int(second)
+# print("The sum is: "+sum)
+print("The sum is: "+str(sum))
