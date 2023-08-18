ae81f1eb83a1fe9d04c24f0b7d70122e83fe2396
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2023-08-18 12:00:32 +0530

    adding py file

---
+++ strings.py
@@ -0,0 +1,11 @@
+name="Tony stark"
+print(name.upper())
+print(name.lower())
+
+print(name.find("s"))
+
+print(name.replace("Tony stark","Ironman"))
+
+print("T" in name)
+print("Tony" in name)
+print("ironman" in name)
