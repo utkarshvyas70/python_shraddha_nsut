53c520e84b685da4ba6d258f682afc73f8004eda
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2023-08-18 12:57:04 +0530

    adding py file

---
+++ operators.py
@@ -0,0 +1,30 @@
+(print(5//2))
+print(5%2)
+print(5**2)
+
+i=5
+i+=2
+i-=2
+i*=2
+
+# operator precedence
+# ((),**,+a,-b,*,/,//,%,+,-,<<,>>)
+
+reuslt=2+3*5
+print(reuslt)
+result=(2+3)*5
+print(reuslt)
+
+#comparison operator
+
+print(3>2)
+print(3<2)
+print(3<=2)
+print(3>=2)
+print(3==3)
+print(3!=3)
+
+# logical operator
+print(2>3 or 2>1)
+print(2<3 and 2>1)
+print(not(2<3))
