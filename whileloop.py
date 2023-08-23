a6917e9c849028265a30076ec0af0aaa880e393c
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2023-08-23 14:07:46 +0530

    adding py file

---
+++ whileloop.py
@@ -0,0 +1,19 @@
+# i=0
+# while(i<3):
+#     print(i)
+#     i=i+1
+
+
+# simple loop
+# i=int(input("Enter the number: "))
+# while (i<32):
+#     i=int(input("Enter the number till you want to: "))
+#     print(i)
+
+# reverse loop with else
+count=55
+while(count>0):
+    print(count)
+    count=count-1
+else:
+    print("I am inside else.")
