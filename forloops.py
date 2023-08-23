8e5e2810af2895b5798fd78fbd15b18d6bcda515
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2023-08-23 13:52:51 +0530

    adding py file

---
+++ forloops.py
@@ -0,0 +1,18 @@
+# iterating a string
+name="Elvish"
+for i in name:
+    print(i)
+
+# iterating a list
+colors=["Red","Green","Blue","Yellow"]
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+
+# range
+for i in range(5):
+    print(i)
+
+for k in range(1,12,2):
+    print(k)
