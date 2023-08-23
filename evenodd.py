3bf801e3b0ad7e5f9b90e1070ee10c8424914629
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2023-08-23 15:09:15 +0530

    adding py file

---
+++ evenodd.py
@@ -0,0 +1,6 @@
+print("Welcome to check the even and odd number.")
+number=int(input("Enter the number you want to check: "))
+if number%2==0:
+    print("This is an even number.")
+if number%2!=0:
+    print("This is an odd number.")
