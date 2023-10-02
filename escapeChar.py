e213f7519fa4484f3b03eb6d914dcfdcd1e3dbcc
Utkarsh Vyas <utkarshvyas200370@gmail.com>
2023-10-02 14:49:56 +0530

    adding py file

---
+++ escapeChar.py
@@ -0,0 +1,7 @@
+txt="We are so called \"Vikings\" from the north"
+txt1="We are so called \'Vikings\' from the north"
+txt2="We are so called \\Vikings\\ from the north"
+print(txt)
+print(txt1)
+print(txt2)
+
